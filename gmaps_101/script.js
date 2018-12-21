25abc775f4a89a656506235b3dc50c5bbb2113b2
rubinoAM <parkcathedralgrace@gmail.com>
2018-12-21 11:53:53 -0500

    Update

--- gmaps_101/script.js
+++ gmaps_101/script.js
@@ -1,7 +1,25 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+    var myLatlng = {lat: 40.641368, lng: -73.570557};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: myLatlng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'What Was Once Home...'
+    });
+
+    map.addListener('center_changed', function() {
+        window.setTimeout(function() {
+        map.panTo(marker.getPosition());
+        }, 3000);
+    });
+
+    marker.addListener('click', function() {
+        map.setZoom(8);
+        map.setCenter(marker.getPosition());
+    });
 }
